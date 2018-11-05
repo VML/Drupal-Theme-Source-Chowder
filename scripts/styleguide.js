a7b20024998fa895a7afa8b94ea3a1f371b94dff
Jacob Berthelsen <jacob.berthelsen@vml.com>
2018-11-05 12:50:35 -0600

    Keep menu closed by default and update some colors

--- scripts/styleguide.js
+++ scripts/styleguide.js
@@ -4,7 +4,7 @@
   Drupal.behaviors.styleguide = {
     attach: function (context, settings) {
       $('.sg-menu').each(function (index) {
-        $('body').addClass('jsa-styleguide-menu jsa-styleguide-menu--open');
+        $('body').addClass('jsa-styleguide-menu');
 
         $('.js-sg-menu-toggle').click(function () {
           $('body').toggleClass('jsa-styleguide-menu--open');
